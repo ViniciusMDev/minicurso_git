a13267a706d01a5dc98f0f613685e09be8359100
ViniciusMDev <contato.vinicius93@gmail.com>
2018-05-13 02:52:07 -0300

    Implementação de modo iterativo

--- fibonacci.c
+++ fibonacci.c
@@ -1,5 +1,13 @@
 #include "fibonacci.h"
 
 int fib(int n){
-  return 0;
+  int i, aux , anterior , f;
+  anterior = 1;
+  f = 1;
+  for (i = 2; i < n; i++) {
+    aux = f;
+    f = f + anterior ;
+    anterior = aux;
+ }
+ return f;
 }
